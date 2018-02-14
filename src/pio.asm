30e2754a424c20fd067d6effc664376658789df2
Mario Blunk <mario.blunk@blunk-electronic.de>
2018-02-14 20:32:15 +0100

    comments added

--- src/pio.asm
+++ src/pio.asm
@@ -1,4 +1,9 @@
- 	ld	A,0CFh
- 	out	(PIO_A_C),A	;set PIO A to bit mode
- 	ld	A,0F8h
- 	out	(PIO_A_C),A	;set io configuration: A[2:0] are outputs
+; Demo Assemby Code
+; PIO initialization
+
+	ld	A,0CFh
+	out	(PIO_A_C),A	;set PIO A to bit mode
+	ld	A,0F8h
+	out	(PIO_A_C),A	;set io configuration: A[2:0] are outputs
+
+; end of file
